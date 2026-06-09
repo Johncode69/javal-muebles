3e0827dccb2ddb77cc18b1a1e253de72804f5fa2
John <jhon@origenstudio.com>
2026-06-09 23:35:49 +0200

    style: use exact percentages for layout shift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ function MaterialShowcase() {
               
               <FadeUp delay={0.1}>
                 <div 
-                  className="relative aspect-[16/9] lg:aspect-[4/5] w-full lg:w-[55%] overflow-hidden bg-[#e8e8e8] mb-4 ml-0 lg:ml-32 mt-4 lg:mt-16"
+                  className="relative aspect-[16/9] lg:aspect-[4/5] w-full lg:w-[55%] overflow-hidden bg-[#e8e8e8] mb-4 ml-0 lg:ml-[15%] mt-4 lg:mt-[5vh]"
                 >
                   <AnimatePresence mode="wait">
                     <motion.div
